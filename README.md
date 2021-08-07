8758f08fa7efb8380d290cf1122d1b3395bfa4f5
Ashley Hardin <ahhardin@gmail.com>
2021-08-07 11:51:39 -0400

    basic server setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # security-breaches-server
+
+## to run server
+```
+> npm install
+> DEBUG=myapp:* npm start
+```
+If nodemon is installed, can instead run the following to live reload
+```
+> nodemon
+```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios').default
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/breaches', async function(req, res, next) {
+  const headers = {
+    'hibp-api-key': '11a561d02d894b5ba7239d6d1500e73a'
+  }
+  const email = 'ahhardin@gmail.com'
+  const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${email}?truncateResponse=false`
+  let breachData = await axios(url, {headers})
+  res.send(breachData.data)
 });
 
 module.exports = router;
