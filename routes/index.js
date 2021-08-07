217ffa474d4605ab885bfe549a1598fc54a6b8c9
Ashley Hardin <ahhardin@gmail.com>
2021-08-07 14:42:51 -0400

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 const axios = require('axios').default
 
 /* GET home page. */
-router.get('/breaches', async function(req, res, next) {
+router.get('/breaches', async function(req, res) {
+  const email = req.query.email
+  if (!email) {
+    res.status(400)
+    res.send({error: 'email address query param required'})
+  }
   const headers = {
     'hibp-api-key': '11a561d02d894b5ba7239d6d1500e73a'
   }
-  const email = 'ahhardin@gmail.com'
-  const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${email}?truncateResponse=false`
+  const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${encodeURIComponent(email)}?truncateResponse=false`
   let breachData = await axios(url, {headers})
   res.send(breachData.data)
 });
